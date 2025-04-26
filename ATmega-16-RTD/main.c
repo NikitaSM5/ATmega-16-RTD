a2f70ac804bd65a507f4abfaf25bfd8a7c8a8752
Nikita <zelnickiy@bk.ru>
2025-04-26 21:17:46 +0300

    Added rtc

--- ATmega-16-RTD/main.c
+++ ATmega-16-RTD/main.c
@@ -1,6 +1,8 @@
 #include "sevseg.h"
 #include "max31865.h"        
 #include "main.h"
+#include "twi.h"
+#include "pcf8583.h"
 
 #define F_CPU 8000000UL
 #include <util/delay.h>
@@ -8,8 +10,11 @@
 #include <math.h>
 #include <avr/interrupt.h>
 #include <avr/io.h>
+#define BTN_MASK (1<<PC3)
+
 
 volatile uint8_t disp_digits[4];
+static inline uint8_t bcd2dec(uint8_t v) { return ((v >> 4) * 10) + (v & 0x0F); }
 
 static void temperature_to_digits_tenth(float t)
 {
@@ -30,27 +35,57 @@ static void temperature_to_digits_tenth(float t)
 	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
 		for (uint8_t i = 0; i < 4; i++) disp_digits[i] = d[i];
 	}
-	sevseg_set_dot(1);                     /* 12.3 -> 1 2·3 */
+	sevseg_set_dot(1);                   
+}
+
+static void time_to_digits_mmss(void)
+{
+	struct pcf_time t;
+	pcf_read_time(&t);
+
+	while (twi_status != TWI_STATUS_RX_COMPLETE) ;
+	twi_status = TWI_STATUS_READY;
+
+	uint8_t sec = bcd2dec(t.seconds);
+	uint8_t min = bcd2dec(t.minutes);
+
+	uint8_t d[4];
+	d[0] = sec % 10;
+	d[1] = sec / 10;
+	d[2] = min % 10;
+	d[3] = min / 10;
+
+	ATOMIC_BLOCK(ATOMIC_RESTORESTATE) {
+		for (uint8_t i = 0; i < 4; i++) disp_digits[i] = d[i];
+	}
+	sevseg_set_dot(2);
 }
 
 int main(void)
 {
 	sevseg_init();
+	twi_init();
 	sevseg_blank_leading(1);                
 	max31865_init(0, 0);
 	DDRB |= (1 << PB4);
 	TCCR0 = (1 << CS01) | (1 << CS00);
 	TIMSK |= (1 << TOIE0);
+	
+	DDRC &= ~BTN_MASK;
+	PORTC |= BTN_MASK;
 	sei();
-	_delay_ms(10000); 
 	while (1) {
-		float temp_c = max31865_read_temperature();
-		temperature_to_digits_tenth(temp_c);
-		_delay_ms(1000);                      /* раз в секунду */
+		if (!(PINC & BTN_MASK)) {
+			time_to_digits_mmss();
+			} else {
+			float temp_c = max31865_read_temperature();
+			temperature_to_digits_tenth(temp_c);
+		}
+		_delay_ms(500);
 	}
 }
 
 ISR(TIMER0_OVF_vect)
 {
-	sevseg_display_process(disp_digits);      /* быстрая мультиплексия */
+	sevseg_display_process(disp_digits);      
 }
